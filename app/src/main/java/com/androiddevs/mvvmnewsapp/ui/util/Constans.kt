5b38af0a6127aee9b3a413d839ff8bd43c9757a1
Ngo Nguyen <ngonguyenvan910@gmail.com>
2022-04-10 13:29:09 +0700

    10/4 retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constans.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constans {
+    companion object{
+        const val API_KEY = "4785d5398aeb4ce69416c4983877507b"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
