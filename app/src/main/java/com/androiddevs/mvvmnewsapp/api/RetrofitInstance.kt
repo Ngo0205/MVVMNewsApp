d943bd428e51621ca64a6656875806ca325c20e3
Ngo Nguyen <ngonguyenvan910@gmail.com>
2022-04-10 23:44:36 +0700

    10/4 Dao and Database

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
@@ -11,9 +11,11 @@ class RetrofitInstance {
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
             val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
                 .build()
+
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
